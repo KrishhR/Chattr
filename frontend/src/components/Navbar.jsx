265c01ba42b9ba7f5dbad0c440be11456f8a3288
Krishnam Rastogi <rastogikrishnam00@gmail.com>
2025-09-23 23:38:55 +0530

    added navbar, loginpage and their functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+    const { authUser, logout } = useAuthStore();
+
+    return (
+        <header className="border-b border-base-300 w-full fixed top-0 z-40 backdrop-blur-lg bg-base-100/80">
+            <div className="container mx-auto px-4 h-16">
+                <div className="flex items-center justify-between h-full">
+
+                    {/* LOGO */}
+                    <div className="flex items-center gap-8">
+                        <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+                            <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                                <MessageSquare className="size-5 text-primary" />
+                            </div>
+                            <h1 className="text-lg font-bold">Chattr</h1>
+                        </Link>
+                    </div>
+
+                    {/* right side of navbar */}
+                    <div className="flex items-center gap-2">
+                        <Link to="/settings" className="btn btn-sm gap-2 transition-colors">
+                            <Settings className="size-4" />
+                            <span className="hidden sm:inline">Settings</span>
+                        </Link>
+
+                        {authUser && (
+                            <>
+                                <Link to="/profile" className="btn btn-sm gap-2 transition-colors">
+                                    <User className="size-4" />
+                                    <span className="hidden sm:inline">Profile</span>
+                                </Link>
+
+                                <button className="btn btn-sm flex gap-2 items-center" onClick={logout}>
+                                    <LogOut className="size-4" />
+                                    <span className="hidden sm:inline">Logout</span>
+                                </button>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Navbar;
