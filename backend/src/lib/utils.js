b2738f9b6054dffa1f8ca30289247cc79daeaad5
Krishnam Rastogi <rastogikrishnam00@gmail.com>
2025-09-23 14:38:02 +0530

    set exiry time to 1 day

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,7 +6,7 @@ export const generateToken = (userId, res) => {
     });
 
     res.cookie("auth_token", token, {
-        maxAge: 60 * 1000, // in milliseconds
+        maxAge: 1 * 24 * 60 * 60 * 1000, // in milliseconds
         httpOnly: true,  // prevent XSS attacks cross-site scripting attacks
         sameSite: true,  // prevent CSRF attacks cross-site request forgery attacks
         secure: process.env.NODE_ENV !== "development"
