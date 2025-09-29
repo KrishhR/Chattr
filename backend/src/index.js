5ba5ecfd3f61bf422920a9f5b69b7207eabe3702
Krishnam Rastogi <rastogikrishnam00@gmail.com>
2025-09-29 20:38:35 +0530

    deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+
 import { app, server } from "./lib/socket.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -12,6 +14,7 @@ import { connectToMongoDB } from "./lib/db.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
@@ -23,9 +26,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", messageRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
