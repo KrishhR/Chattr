28492d091db521d2fb88856d871ca32c0faf041b
Krishnam Rastogi <rastogikrishnam00@gmail.com>
2026-03-18 10:46:56 +0530

    Fix static file serving path and update package.json scripts

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,18 +27,18 @@ app.use(cors({
     credentials: true
 }));
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json()); // To parse JSON data in the req.body
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", messageRoutes);
-// app.get("/ping", (req, res) => res.status(200).send("pong"));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
+    // frontend(react) app
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
     });
 }
 
